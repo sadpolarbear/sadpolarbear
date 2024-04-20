c785949997273aa7a4fecaba3dfb4811e2d41fbd
yarkiy keramic <101712572+sadpolarbear@users.noreply.github.com>
2024-04-20 09:06:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function generateProgressBar() {
 }
 
 const readme = `\
+### Hi there 👋
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ---
 ⏰ Updated on ${new Date().toUTCString()}
